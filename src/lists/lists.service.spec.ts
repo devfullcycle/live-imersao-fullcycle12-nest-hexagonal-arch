20c4d6f60d2587f16cebd0c1cbd3c6321f355826
Luiz Carlos <argentinaluiz@gmail.com>
2023-01-31 22:23:52 -0300

    add more tests

--- src/lists/lists.service.spec.ts
+++ src/lists/lists.service.spec.ts
@@ -3,25 +3,26 @@ import { ListsService } from './lists.service';
 import { List } from './entities/list.entity';
 import { ListGatewayInMemory } from './gateways/list-gateway-in-memory';
 import { of } from 'rxjs';
-
-const mockHttpService = {
-  post: jest.fn().mockReturnValue(of(null)),
-};
+import { ListCreatedEvent } from './events/list-created.event';
 
 describe('ListsService', () => {
   let service: ListsService;
   let listPersistenceGateway: ListGatewayInMemory;
-  let listIntegrationGateway: ListGatewayInMemory;
+  const eventEmitterMock = {
+    emit: jest.fn(),
+  };
   beforeEach(() => {
     listPersistenceGateway = new ListGatewayInMemory();
-    listIntegrationGateway = new ListGatewayInMemory();
-    service = new ListsService(listPersistenceGateway, listIntegrationGateway);
+    service = new ListsService(listPersistenceGateway, eventEmitterMock as any);
   });
 
   it('deve criar uma lista', async () => {
     const list = await service.create({ name: 'my list' });
     expect(listPersistenceGateway.items).toEqual([list]);
-    expect(listIntegrationGateway.items).toEqual([list]);
+    expect(eventEmitterMock.emit).toHaveBeenCalledWith(
+      'list.created',
+      new ListCreatedEvent(list),
+    );
   });
   // let service: ListsService;
 
